dd1eae3a291a4201e66b6704e79cebe8665ec967
Dinh Nguyen Nhat Quang <quangdinh0106@gmail.com>
2025-03-14 17:43:35 +0700

    ...

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.use(controller);
 
 app.set("views", __dirname + "/apps/views");
 app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
 app.use("/partical", express.static(__dirname + "/views/partical"));
+
+var bodyParser = require('body-parser')
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 var server = app.listen(3000, function()
 {
