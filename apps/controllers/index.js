e48a1293705a93ae700a3a7992601cbbbf4f2a69
Dinh Nguyen Nhat Quang <quangdinh0106@gmail.com>
2025-03-14 18:05:14 +0700

    FE

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.use("/home", require(__dirname + "/HomeController"));
+router.use("/contact", require(__dirname + "/ContactController"));
+router.use("/single-blog", require(__dirname + "/SingleBlogController"));
+router.use("/review", require(__dirname + "/ReviewController"));
+router.use("/categories", require(__dirname + "/CategoriesController"));
 
 router.get("/", function (req, res) {
     res.render('index.ejs');
