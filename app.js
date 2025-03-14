0ede9d27adcbc8b6873794da667921b044b63662
Dinh Nguyen Nhat Quang <quangdinh0106@gmail.com>
2025-03-14 17:17:52 +0700

    ...

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var app = express();
+
 var controller = require(__dirname  + "/apps/controllers");
 app.use(controller);
+
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use("/partical", express.static(__dirname + "/views/partical"));
+
 var server = app.listen(3000, function()
 {
     console.log("server is running");
