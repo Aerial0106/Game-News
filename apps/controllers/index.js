b162ed6f940b217dd27d9e33cd91e60f7534fde3
haihai712.io <kyfer712@gmail.com>
2025-04-01 09:36:04 +0700

    ...

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -9,6 +9,15 @@ router.use("/review", require(__dirname + "/ReviewController"));
 router.use("/categories", require(__dirname + "/CategoriesController"));
 router.use("/community", require(__dirname + "/CommunityController"));
 
+//Admin
+router.use("/admin", require(__dirname + "/admin/AdminController"));
+router.use("/widgets", require(__dirname + "/admin/WidgetsController"));
+router.use("/forms", require(__dirname + "/admin/Formscontroller"));
+router.use("/sidebar-style-2", require(__dirname + "/admin/sidebarlayout/SidebarController"));
+router.use("/icon-menu", require(__dirname + "/admin/sidebarlayout/Icon-menuController"));
+router.use("/charts", require(__dirname + "/admin/charts/Chartscontroller"));
+router.use("/sparkline", require(__dirname + "/admin/charts/Sparklinecontroller"));
+
 router.get('/auth/register', AuthController.showRegister);
 router.post('/auth/register', AuthController.register);
 router.get('/auth/login', AuthController.showLogin);
